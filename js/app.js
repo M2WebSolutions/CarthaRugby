7ab48ad69b085238031e49a85f6ed6e1b39512cb
CairnMacGregor <60977182+CairnMacGregor@users.noreply.github.com>
2021-01-14 13:21:52 +0000

    Fixes

--- js/app.js
+++ js/app.js
@@ -5,6 +5,15 @@ toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle('active')
 });
 
-const navBar = document.getElementsByClassName("navbar")[0];
+const navBar = document.getElementById("navbar")
 
-navbarLinks.style.background = "White"
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    navBar.style.marginTop = "0px";
+  } else {
+    navBar.style.marginTop = "40px";
+  }
+}
