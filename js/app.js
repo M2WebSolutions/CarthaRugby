cab05b96cbe499db3f3c9c32ce86516d3f43083b
CairnMacGregor <60977182+CairnMacGregor@users.noreply.github.com>
2021-01-14 13:54:00 +0000

    Added functionailty and changed hero

--- js/app.js
+++ js/app.js
@@ -13,9 +13,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     navBar.style.marginTop = "0px";
+
+    navBar.style.transition = "all 0.3s ease-in-out";
   } else {
     navBar.style.marginTop = "40px";
-    navBar.style.transition = "all 0.3s ease-in-out";
 
+    navBar.style.transition = "all 0.3s ease-in-out";
   }
 }
